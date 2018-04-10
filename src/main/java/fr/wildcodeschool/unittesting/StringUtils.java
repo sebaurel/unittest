7fe2d1693045b91888b1f762b5464c7877a9b75b
sebaurel <sebaurel@free.fr>
2018-04-10 16:00:17 +0200

    Pass all tests

--- src/main/java/fr/wildcodeschool/unittesting/StringUtils.java
+++ src/main/java/fr/wildcodeschool/unittesting/StringUtils.java
@@ -1,7 +1,7 @@
 package fr.wildcodeschool.unittesting;
 
 public class StringUtils {
-    private static final String ALL_VOWELS = "aeiouAEIOU";
+    private static final String ALL_VOWELS = "aeiouyAEIOUY";
 
     /**
      * Renvoie la chaine formée par les voyelles d'une chaine de caractères
@@ -9,10 +9,12 @@ public class StringUtils {
      */
     public static String vowels(String candidate) {
         String vowels = "";
-        char[] letters = candidate.toCharArray();
-        for (int i = 1; i < candidate.length(); i++) {
-            if (ALL_VOWELS.indexOf(letters[i]) >= 0) {
-                vowels += letters[i];
+        if (candidate != null){
+            char[] letters = candidate.toCharArray();
+            for (int i = 0; i < candidate.length(); i++) {
+                if (ALL_VOWELS.indexOf(letters[i]) >= 0) {
+                    vowels += letters[i];
+                }
             }
         }
         return vowels;
--- src/test/java/fr/wildcodeschool/unittesting/StringUtilsTest.java
+++ src/test/java/fr/wildcodeschool/unittesting/StringUtilsTest.java
@@ -2,29 +2,59 @@ package fr.wildcodeschool.unittesting;
 
 
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringUtilsTest {
 
-    String pourTestCaractereCapitale = new fr.wildcodeschool.unittesting.StringUtils().vowels("Etest123");
-    String pourTestCaractereAccentue = new fr.wildcodeschool.unittesting.StringUtils().vowels("é");
-    String pourTestCaractereIndefini = new fr.wildcodeschool.unittesting.StringUtils().vowels(null);
+    public StringUtils listeString;
 
+    @BeforeEach
+    public void initialisation(){
+        StringUtils listeString = new StringUtils();
+    }
 
     @Test
-    public void caractereCapitale (){
-        assertEquals("Ee", pourTestCaractereCapitale);
+    public void voyellePremiereLettre (){
+        assertEquals("Eea", StringUtils.vowels("Eerta"));
     }
 
 
     @Test
-    public void chaineAccentue(){
-        assertEquals("é", pourTestCaractereAccentue);
+    public void caractereIndefini(){
+        assertEquals("", StringUtils.vowels(null));
     }
-
 
     @Test
-    public void chaineIndefini(){
-        assertEquals("", pourTestCaractereIndefini);
+    public void seulementVoyelle(){
+        assertEquals("aeiouy", StringUtils.vowels("aeiouy"));
     }
+
+    @Test
+    public void voyelleDerniereLettre(){
+        assertEquals("y", StringUtils.vowels("pfjgky"));
+    }
+
+    @Test
+    public void pasDeVoyelle(){
+        assertEquals("", StringUtils.vowels("hdjfklsm"));
+    }
+
+    @Test
+    public void longueChaine(){
+        assertEquals("iuoioyuiyuioiueaeiouayy", StringUtils.vowels("dfrtiuoplkiojhgtyuidhgyuiklmpoiufdsetaeissouggmlpatysy"));
+    }
+
+    @Test
+    public void unCaractereVoyelle(){
+        assertEquals("a", StringUtils.vowels("a"));
+    }
+
+    @Test
+    public void unCaractereConsonne(){
+        assertEquals("", StringUtils.vowels("D"));
+    }
+
+
 }
